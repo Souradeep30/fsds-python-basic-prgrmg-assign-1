97a7072b2ee42c1fac5350e9e935b754960f3199
Souradeep30 <95559269+Souradeep30@users.noreply.github.com>
2022-02-27 23:31:13 +0530

    Add files via upload

---
+++ python programming basic assignment 1 (1).py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Answer 1:
+
+print("hello python")
+
+
+# In[3]:
+
+
+#Answer 2:
+
+a=30
+b=10
+
+c=a+b
+d=a/b
+print("the summation a & b is",c)
+print("the division of a & b is",d)
+
+
+# In[16]:
+
+
+#Answer 3:
+
+base=10
+height=20
+area= base*height/2
+
+print ("the area of the triangle is", area,)
+
+
+# In[17]:
+
+
+#Answer 4:
+
+var1=123
+var2=456
+print('Before swap:\nvar1 = {} and var2 = {}'.format(var1, var2))
+temp = var1
+var1 = var2
+var2 = temp
+print('\nAfter swap:\nvar1 = {} and var2 = {}'.format(var1, var2))
+
+
+# In[18]:
+
+
+#answer 5:
+
+import random
+
+print(random.random())
+print(random.randint(1, 5000))
+
+
+# In[ ]:
+
+
+
+
